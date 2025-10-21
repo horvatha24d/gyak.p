cd4a990f0886b8f516e6e19c4428c7fe042f03c7
horvatha24d <horvatha.24d@acsjszki.hu>
2025-10-21 09:22:34 +0200

    g

--- harmadik.py
+++ harmadik.py
@@ -5,4 +5,15 @@ if szam1   <=szam2 and szam1<= szam3:
     if szam2<=szam3:
       print(f"A számok növekvő sorrendben: {szam1},{szam2},{szam3}")
     else:
-    print(f"A számok növekvő sorrrendben:{szam1},{szam3},
+      print(f"A számok növekvő sorrrendben:{szam1},{szam3},{szam2}")
+elif szam2 <=szam1 and szam2<=szam3:
+   if szam1<=szam3:
+      print(f"A számok növekvő sorrendben: {szam2},{szam1},{szam3}")
+   else:
+      print(f"A számok növekvő sorrendben:{szam2},{szam3},{szam1}")
+else:
+   if szam1<=szam2:
+      print(f"A számok növekvő sorrendben: { szam3},{szam1},{szam2}")
+   else:
+      print(f"A számok növekvő sorrendben: {szam3}.{szam2},{szam1}")
+
