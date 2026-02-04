4e74e1515901639f0d0ab9d4048f545d042ab9c9
horvatha24d <horvatha.24d@acsjszki.hu>
2026-02-04 08:24:19 +0100

    fhsjk

--- j/fajlkezeles.py
+++ j/fajlkezeles.py
@@ -1,4 +1,4 @@
-fajl=open("j/adatok.txt","r",encoding='UTF-8')
+'''fajl=open("j/adatok.txt","r",encoding='UTF-8')
 
 
 tartalom=fajl.read()
@@ -7,3 +7,17 @@ print(tartalom)
 
 
 fajl.close()
+'''
+
+
+
+fajl =open('j/adat.txt','w',encoding='UTF-8')
+
+
+fajl.write('Helló Világ')
+
+
+
+
+fajl.close()
+
