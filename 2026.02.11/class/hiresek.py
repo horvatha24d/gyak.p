79fcaef4f172ce4df26b9f3d4292cdeb6fe757f7
horvatha24d <horvatha.24d@acsjszki.hu>
2026-02-17 14:38:55 +0100

    Update hiresek.py

--- 2026.02.11/class/hiresek.py
+++ 2026.02.11/class/hiresek.py
@@ -1,4 +1,4 @@
-from hiresek_alap import HíresNő
+'''from hiresek_alap import HíresNő
 
 hiresek = []
 
@@ -11,5 +11,30 @@ for i in range(3):
     hiresek.append(nő)
 
 for nő in hiresek:
-    print(f"{nő.előtag()} {nő.név} egy híres {nő.foglalkozás}")
+    print(f"{nő.előtag()} {nő.név} egy híres {nő.foglalkozás}")'''
 
+
+class HíresNő:
+    def __init__(self, név, foglalkozás, nemzetiség):
+        self.név = név
+        self.foglalkozás = foglalkozás
+        self.nemzetiség = nemzetiség
+
+    def elötag(self):
+        if self.nemzetiség == "a":
+            return "Ms."
+        else:
+            return 'Frau'
+
+
+#----------------------------------------------------------#
+hiresnok=[]
+for i in range(3):
+    nev=input("Add meg egy hires no nevet:")
+    foglalkozas=input("Add meg a foglalkozasat:")
+    nemzetiseg=input("Add meg a nemzetiseget (a/n):")
+    hiresno=HíresNő(nev,foglalkozas,nemzetiseg)
+    hiresnok.append(hiresno)
+
+for elem in hiresnok:
+    print(elem.elötag(), elem.név, elem.foglalkozás)
