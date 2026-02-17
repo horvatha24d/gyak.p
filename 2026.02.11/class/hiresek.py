9a2fe2acc2c86741410420c4d0d8c081507cb4e8
horvatha24d <horvatha.24d@acsjszki.hu>
2026-02-17 14:12:49 +0100

    h

---
+++ 2026.02.11/class/hiresek.py
@@ -0,0 +1,16 @@
+
+from hiresek_alap import HíresNő
+
+hiresek = []
+
+for i in range(3):
+    név = input("Add meg egy híres nő nevét! ")
+    foglalkozás = input("Add meg a foglalkozását! ")
+    nemzetiség = input("Add meg a nemzetiségét (a/n)! ")
+
+    nő = HíresNő(név, foglalkozás, nemzetiség)
+    hiresek.append(nő)
+
+for nő in hiresek:
+    print(f"{nő.előtag()} {nő.név} egy híres {nő.foglalkozás}")
+
