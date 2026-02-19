f670ee4e6b4a0be5bedba96d5a6825784f849ee3
horvatha24d <horvatha.24d@acsjszki.hu>
2026-02-19 13:13:17 +0100

    k

--- 2026.02.11/class/0219/class.py
+++ 2026.02.11/class/0219/class.py
@@ -1,44 +1,35 @@
-class magassag:
-    def __init__(self, nev, cm):
-        self.nev = nev
-        self.cm = cm
-
-f = open("2026.02.11/class/0219/diak.txt", "r", encoding="utf-8")
-adatok = f.read()
-f.close()
-
-w=print(adatok)
-
-adatok = adatok.split("\n")
-diakok = []
-db = 0
-
-for sor in adatok:
-    if sor.strip():
-        nev, cm = sor.strip().split(";")
-        diak = magassag(nev, int(cm))
-        diakok.append(diak)
-        db += 1
-
-d=print(f"A diákok száma: {db} fő")
-
-# Legmagasabb diák(ok) enumerate nélkül
-max_cm = max(diak.cm for diak in diakok)
-legmagasabbak = [diak.nev for diak in diakok if diak.cm == max_cm]
-x=print(f"Legmagasabb diák: ", end="")
-i = 0
-while i < len(legmagasabbak):
-    if i > 0:
-        print(", ", end="")
-    print(legmagasabbak[i], end="")
-    i += 1
-k=print(f" ({max_cm} cm)")  # While ciklusos kiírás [file:1]
-
-# Átlagmagasság
+class Diak:
+    def __init__(self,nev,magassag):
+        self.nev=nev
+        self.magassag=magassag
 
 
-# Legkisebb magasság
-min_cm = min(diak.cm for diak in diakok)
-n=print(f"Legalacsonyabb: {min_cm} cm")
+#fálj feldoolgozása
 
-wfajl = open("2026.02.11/class/0219/eredmenyek.txt", "w", encoding="utf-8")
+
+fajl=open("2026.02.11/class/0219/diak.txt","r",encoding="UTF-8")
+#soronkent olvasás
+tartalom=fajl.read()
+Ltartalom=tartalom.split("\n")
+LLtartalim=[]
+for sor in Ltartalom:
+    Ldarabok=sor.split(";")
+    nev=Ldarabok[0]
+    magassag=int(Ldarabok[1])
+    diak=Diak(nev,magassag)
+    LLtartalim.append(diak)
+
+#adaatok ellenőrzése
+for diak in LLtartalim:
+    print(diak.nev,)
+
+#Elso feladat:diakok szama
+
+print("Diákok száma:", len(LLtartalim),"fő")
+#Második feladat: legmagasabb   
+
+legmagasabb=LLtartalim[0]
+for diak in LLtartalim:
+    if diak.magassag>legmagasabb.magassag:
+        legmagasabb=diak
+print("A legmagasabb diák:", legmagasabb.nev, legmagasabb.magassag,"cm")
